1dbb378b78ce8297a01d5a8f08ca79b78f317c57
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-21 22:46:52 -0400

    split out index router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const path = require("node:path");
 const express = require("express");
 const app = express();
+const indexRouter = require("./routes/index");
 const assetsPath = path.join(__dirname, "public");
 
 app.use(express.static(assetsPath));
@@ -9,9 +10,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.use("/", indexRouter);
 
 app.listen(3000, (error) => {
   if (error) {
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", async (req, res, next) => {
+  if (!req.user) {
+    return res.render("home");
+  }
+
+  next();
+
+  const errorMessage =
+    req.query.error === "bad_password"
+      ? "Incorrect secret password. Try again."
+      : null;
+
+  res.render("index", {
+    user: req.user,
+    error: errorMessage,
+  });
+});
+
+module.exports = router;
