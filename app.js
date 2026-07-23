bba3d74ee0fb30a3de98b1781a92a28bf5864917
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-22 21:55:41 -0400

    remove duplicate session configs

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ require("dotenv").config();
 const path = require("node:path");
 const express = require("express");
 const app = express();
-const session = require("express-session");
 const passport = require("passport");
 const sessionConfig = require("./config/sessionConfig");
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
 const assetsPath = path.join(__dirname, "public");
 
-app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
-app.use(passport.session());
+app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: false }));
+
 app.use(sessionConfig);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/", authRouter);
 
