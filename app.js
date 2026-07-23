8cd7549547b86e34ec7a9f9e1dd8a1e66d258c3a
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-22 21:17:02 -0400

    enter required variables to app.js to use passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const path = require("node:path");
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const passport = require("passport");
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
 const assetsPath = path.join(__dirname, "public");
@@ -9,6 +11,9 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
