4252a6b847eecf81ebbd97ac2d9ead7aff01c2be
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-23 00:08:04 -0400

    add folders to route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-
+const prisma = require("../database/prismaClient.js");
 const router = Router();
+const { EntityType } = require("../generated/prisma/client.js");
 
 router.get("/", async (req, res, next) => {
   if (!req.user) {
@@ -9,8 +10,15 @@ router.get("/", async (req, res, next) => {
 
   next();
 
+  const folders = await prisma.entity.findMany({
+    where: {
+      userId: req.user.id,
+      type: EntityType.FOLDER,
+    },
+  });
   res.render("index", {
     user: req.user,
+    folders: folders,
   });
 });
 
